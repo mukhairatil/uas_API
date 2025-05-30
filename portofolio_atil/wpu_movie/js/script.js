f4c9c192b8efae19d01d971c00d47c69a49cf132
mukhairatil <mukhairatil24@gmail.com>
2025-05-30 18:44:37 +0700

    eror film perbaikan

--- portofolio_atil/wpu_movie/js/script.js
+++ portofolio_atil/wpu_movie/js/script.js
@@ -1,93 +1,102 @@
-function searchMovie() {
-    $('#movie-list').html('');
-    
-    $.ajax({
-        url: 'http://www.omdbapi.com', // pakai www
-        type: 'get',
-        dataType: 'json',
-        data: {
-            'apikey': '73c64296',
-            's': $('#search-input').val()
-        },
-        success: function(result) {
-            if (result.Response == "True") {
-                let movies = result.Search;
-                console.log(movies);
+function searchMovies(keyword) {
+  $('#movie-list').html('');
 
-                $.each(movies, function(i, data) {
-                    $('#movie-list').append(`
-                        <div class="col-md-4 mb-3">
-                            <div class="card">
-                                <img src="${data.Poster}" class="card-img-top" alt="${data.Title}">
-                                <div class="card-body">
-                                    <h5 class="card-title">${data.Title}</h5>
-                                    <h6 class="card-subtitle mb-2 text-muted">${data.Year}</h6>
-                                    <a href="#" class="see-detail text-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="${data.imdbID}">See Detail</a>
-                                </div>
-                            </div>
-                        </div>
-                    `);
-                });
+  if (keyword === '') {
+    $('#recommended-container').show();
+    $('#movie-list').hide();
+    loadRecommendedMovies();
+    return;
+  }
 
-                $('#search-input').val('');
-            } else {
-                $('#movie-list').html(`
-                    <div class="col">
-                        <h1 class="text-center text-danger">${result.Error}</h1>
-                    </div>
-                `);
-            }
-        }
-    });
+  $('#recommended-container').hide();
+  $('#movie-list').show();
+
+  $.ajax({
+    url: 'https://www.omdbapi.com',
+    type: 'get',
+    dataType: 'json',
+    data: {
+      apikey: '73c64296',
+      s: keyword
+    },
+    success: function (response) {
+      if (response.Response === "True") {
+        const movies = response.Search;
+        $('#movie-list').html('');
+        movies.forEach(movie => {
+          $('#movie-list').append(`
+            <div class="col-md-3 mb-3">
+              <div class="card">
+                <img src="${movie.Poster !== "N/A" ? movie.Poster : 'https://via.placeholder.com/300x450?text=No+Image'}" class="card-img-top" alt="${movie.Title}">
+                <div class="card-body">
+                  <h5 class="card-title">${movie.Title}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">${movie.Year}</h6>
+                  <a href="#" class="see-detail text-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="${movie.imdbID}">See Detail</a>
+                </div>
+              </div>
+            </div>
+          `);
+        });
+      } else {
+        $('#movie-list').html(`<p class="text-center">Movie not found!</p>`);
+        $('#recommended-container').show();
+      }
+    },
+    error: function () {
+      $('#movie-list').html(`<p class="text-center text-danger">Failed to fetch data!</p>`);
+      $('#recommended-container').show();
+    }
+  });
 }
 
-$('#search-button').on('click', function () {
-    searchMovie();
-});
+// ✅ Event handler tombol "See Detail"
+$(document).on('click', '.see-detail', function () {
+  const imdbID = $(this).data('id');
 
-$('#search-input').on('keyup', function (e) {
-    if (e.keyCode === 13) {
-        searchMovie();
+  // 🔒 Cegah request kalau imdbID kosong/null/undefined
+if (!imdbID || typeof imdbID !== 'string' || imdbID.trim() === '') {
+  console.warn('IMDb ID kosong, permintaan dibatalkan.');
+  return;
+}
+
+$.ajax({
+  url: 'https://www.omdbapi.com',
+  type: 'get',
+  dataType: 'json',
+  data: {
+    apikey: '73c64296',
+    i: imdbID
+  },
+  success: function (movie) {
+    if (movie.Response === "True") {
+      $('.modal-body').html(`
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-4">
+              <img src="${movie.Poster !== "N/A" ? movie.Poster : 'https://via.placeholder.com/300x450?text=No+Image'}" class="img-fluid" alt="${movie.Title}">
+            </div>
+            <div class="col-md-8">
+              <ul class="list-group">
+                <li class="list-group-item"><h4>${movie.Title}</h4></li>
+                <li class="list-group-item">Released: ${movie.Released}</li>
+                <li class="list-group-item">Genre: ${movie.Genre}</li>
+                <li class="list-group-item">Director: ${movie.Director}</li>
+                <li class="list-group-item">Actors: ${movie.Actors}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      `);
+    } else {
+      $('#movie-list').html(`
+        <div class="col-12">
+          <p class="text-center text-danger fw-bold my-4">Movie not found!</p>
+        </div>
+      `);
+      // Jangan tampilkan rekomendasi ulang saat tidak ditemukan hasil
     }
+  },
+  error: function() {
+    $('.modal-body').html(`<p class="text-danger">Gagal mengambil data detail movie.</p>`);
+  }
 });
-
-$('#movie-list').on('click', '.see-detail', function () {
-    
-    $.ajax({
-        url: 'http://www.omdbapi.com', 
-        type: 'get',
-        dataType: 'json',
-        data: {
-            'apikey': '73c64296',
-            'i': $(this).data('id')
-        },
-        success : function (movie) {
-            if (movie.Response === "True") {
-
-                $('.modal-body').html(`
-                    <div class = "container-fluid">
-                        <div class="row">
-                            <div class = "col-md-4">
-                             <img src = "`+ movie.Poster +`" class="img-fluid">
-                            </div>
-
-                            <div class="col-md-8">
-                                <ul class="list-group">
-                                    <li class="list-group-item"><h3>`+ movie.Title +`</li>
-                                    <li class="list-group-item">Released : `+ movie.Released +`</li>
-                                    <li class="list-group-item">Genre : `+ movie.Genre +`</li>
-                                    <li class="list-group-item">Director : `+ movie.Director +`</li>
-                                    <li class="list-group-item">Actors : `+ movie.Actors +`</li>
-                                    
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-
-                `)
-
-            }
-        }
-    });
-});
-
