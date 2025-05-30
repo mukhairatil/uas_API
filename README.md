97edeb7c0893b154f6a80fe7341213c8b70e88df
mukhairatil <mukhairatil24@gmail.com>
2025-05-30 17:48:19 +0700

    perbaikan key deepseek

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # key omdb = 73c64296
 # kode api yt = AIzaSyAoCv-dBIjagpMtmG0p7K8-DOWa4kLIDsY
-# key deepseek = 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
+# key deepseek = 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
--- portofolio_atil/chatbot_deepseek/chatbot.php
+++ portofolio_atil/chatbot_deepseek/chatbot.php
@@ -7,7 +7,7 @@ header('Content-Type: application/json');
 $input = json_decode(file_get_contents('php://input'), true);
 $userMessage = $input['message'] ?? '';
 
-$apiKey = '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';
+$apiKey = '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';
 $url = $url = 'https://openrouter.ai/api/v1/chat/completions';
 
 
